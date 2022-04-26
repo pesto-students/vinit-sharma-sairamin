a20ca063244412d8444602dcb4daca90ed6a106f
devvinit <vinit91.dev@gmail.com>
2022-04-26 21:30:12 +0530

    updated with menaingful comments and initializers

--- Week-4/assignment2.js
+++ Week-4/assignment2.js
@@ -4,23 +4,31 @@ Create an object calledTeacherderived from thePersonclass, and implement a metho
 [teacher's name]is now teaching[subject]
  **/
 
+//Person function-constructor
 var Person = function() {};
 
+//prototype of Person getting initialize() method
 Person.prototype.initialize = function(name, age)
 {
     this.name = name;
     this.age = age;
 }
 
+//Teacher function-constructor
 function Teacher(){};
 
+//Teacher's protype getting method from Person's prototype
 Teacher.prototype = Person.prototype;
 
+//prototype of Teacher getting teach() method
 Teacher.prototype.teach = function(subject){
     return `${this.name} is now teaching ${subject}`;
 }
 
+//incetenciating from Teacher() constroctor
 var him = new Teacher();
+
 him.initialize("Adam",45);
+
 var result = him.teach("Inheritance");
-console.log(result); 
+console.log(result); //Adam is now teaching Inheritance
