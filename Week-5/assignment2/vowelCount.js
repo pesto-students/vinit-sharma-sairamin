e491728bd0a523136332026977035cd2c3669da9
devvinit <vinit91.dev@gmail.com>
2022-05-08 13:10:13 +0530

    refactored the code to reduce space and time complaxity

--- Week-5/assignment2/vowelCount.js
+++ Week-5/assignment2/vowelCount.js
@@ -1,31 +1,28 @@
 /** Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string. **/
 
 
-function countVowel(char){
+
+function vowelCount(char){
     const vowel = 'aeiou'; 
     
-    let countVowel = new Map();
+    let countVowelMap = new Map();
     const lowerCaseChar = char.toLowerCase();
 
     for(var i=0; i<lowerCaseChar.length; i++){
-        for(var j=0; j<vowel.length; j++){
-            if(vowel[j] === char[i]){
-                if(countVowel.has(j)){      
-                    let mapVal = countVowel.get(j);
-                    countVowel.set(j, mapVal + 1);
+        if(vowel.includes(lowerCaseChar[i])){ //check if aeiou has perticular char
+                let matchedVowel = lowerCaseChar[i]; //getting that matched char of vowel
+                if(countVowelMap.has(vowel.indexOf(matchedVowel))){ //then check matched char's index from vowel
+                    countVowelMap.set(vowel.indexOf(matchedVowel), countVowelMap.get(vowel.indexOf(matchedVowel)) + 1); //char's index from "vowel" will be key
                 }
                 else {
-
-                    countVowel.set(j, 1);
+                    countVowelMap.set(vowel.indexOf(matchedVowel), 1); 
                 }
                 
             }
-        }
+        
     }
-    return countVowel;
+    return countVowelMap;
 }
 
-const vowel = countVowel("aeaaiioiu");
+const vowel = vowelCount("aeaaiioiu");
 console.log(vowel); //Map(5) { 0 => 3, 1 => 1, 2 => 3, 3 => 1, 4 => 1 }
-
-
