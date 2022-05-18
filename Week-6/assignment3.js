30181ead23cc4125a22eb16a53664d3709ed2248
devvinit <vinit91.dev@gmail.com>
2022-05-18 16:51:52 +0530

    time complexity reduced to o(n) from o(n^2)

--- Week-6/assignment3.js
+++ Week-6/assignment3.js
@@ -3,22 +3,32 @@
 
 
 //function to arrange array of 0, 1, 2 into accending order
+//time complexity is reduced with this algorithem.
 function ascendingOrder(ar){
-    for(var j=0; j<ar.length; j++){
-        for(var i=0; i<ar.length; i++){
+    var count=0;
+    var size = ar.length;
+    var sorted=false;
+    while(!sorted){  
+        sorted=true;    
+        for(var i=0; i<size - 1; i++){
+            count++;
             if(ar[i] > ar[i + 1]){
                 var extra = ar[i];
                 ar[i] = ar[i + 1];
                 ar[i + 1] = extra;
+                sorted=false;
             }
         }
+      size--;
     }
     
     return ar;
 }
 
-//time complexity is o(n^2);
+//time complexity is now o(n * n-1/2) which can be concluded to o(n);
 //space complaxity is o(1)
+//execution time reduced to 8.872ms from previous default: 10.517ms
+
 
 var arr = [0, 2, 1, 2, 0];
 console.log(ascendingOrder(arr)); // [ 0, 0, 1, 2, 2 ]
