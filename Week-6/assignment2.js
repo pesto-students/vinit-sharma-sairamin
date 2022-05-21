66b7e020d06748301f7a534738c50d466f4bf3cf
devvinit <vinit91.dev@gmail.com>
2022-05-21 13:52:11 +0530

    Improved the algorithm for n number of inputs

--- Week-6/assignment2.js
+++ Week-6/assignment2.js
@@ -1,29 +1,47 @@
 //Problem 6.2 Spiral Order Matrix II
 //Problem Description Given a matrix of m * n elements (m rows, n columns), return allelements of the matrix in spiral order.Example: Given the following matrix: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ] You should return[1, 2, 3, 6, 9, 8, 7, 4, 5]
 
-//function to arrays into spiral order
-function matrix(arr1, arr2, arr3){
-    var arr2LastElement = arr2[arr2.length - 1]; //getting last el of arr2
 
-    //getting arr2 element without last element
-    var arr2ElementWitoutLast = arr2.splice(0, arr2.length - 1);
-    arr3.reverse(); //reversing the arr3
-    
-    //arranged array and values in the perticular order
-    var spiralList = [...arr1, arr2LastElement, ...arr3, ...arr2ElementWitoutLast ];
-    return spiralList;
+//funtion to create list of spiral-order from M*N matrix 
+function spiralOrder(matrix){
+    let top = 0
+    let left = 0;
+    let bottom = matrix.length - 1;
+    let right = matrix[0].length - 1;;
+    let result = [];
+    let size = matrix.length * matrix[0].length;
+
+    while(result.length < size){
+        for(let i=left; i <= right && result.length < size; i++){
+            result.push(matrix[top][i]);
+        }
+        top++;
+        
+        for(let i=top; i <= bottom && result.length < size; i++){
+            result.push(matrix[i][right]);
+        }
+        right--;
+        
+        for(let i=right; i >= left && result.length < size; i--){
+            result.push(matrix[bottom][i]);
+        }
+        bottom--;
+
+        for(let i=bottom; i >= top && result.length < size; i--){
+            result.push(matrix[i][left]);
+        }
+        left++;
+    }
+    return result;
 }
 
-//time complexity is o(n);
-//space complexity is o(1);
+//time comlexity is o(n)
+//space complexity is o(n)
 
-var arr1 = [ 1, 2, 3 ];
-var arr2 = [ 4, 5, 6 ];
-var arr3 = [ 7, 8, 9 ];
+var arr = [[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]];
+console.log(spiralOrder(arr));
+//output - [ 1, 2, 3, 6, 9, 8, 7, 4, 5 ]
 
-var arr4 = [ 1, 2, 6, 7];
-var arr5 = [ 34, 50, 2, 90 ];
-var arr6 = [ 78, 84, 92, 45 ];
-
-console.log(matrix(arr1, arr2, arr3));
-console.log(matrix(arr4, arr5, arr6));
+var arr2 = [[ 1, 2, 3, 4], [  5, 6, 7, 8 ], [ 9, 10, 11, 12 ], [ 13, 14, 15, 16 ], [ 17, 18, 19, 20 ], [ 21, 22, 23, 24 ]];
+console.log(spiralOrder(arr2));
+//output - [ 1,  2,  3,  4,  8, 12, 16, 20, 24, 23, 22, 21, 17, 13,  9,  5, 6,  7, 11, 15, 19, 18, 14, 10 ]
