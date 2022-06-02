002417d4edc9a05a4b22478f5c15f14efb2d4bdf
devvinit <vinit91.dev@gmail.com>
2022-06-02 16:36:49 +0530

    enhanced the code readability

--- Week-7/assignment4.js
+++ Week-7/assignment4.js
@@ -1,5 +1,6 @@
 //Problem 7.4: Parenthesis Checker
 
+//to solve Parenthesis Checker problem I used stack. I took an array in which I push current parenthesis when it doesn't match with the parenthesis is in object but when they match I pop out the parenthesis from that array. so when function returns emty array it means parenthesis are in perfect formation and when function returns array with values then it means parenthesis are not in perfect formation. 
 
 //checking the perfect formation of parenthesis
 function parenthesisChecker(x){
@@ -26,10 +27,10 @@ function parenthesisChecker(x){
 }
 
 //Input values
-var x = "{{}}"; //srt = {()
-var y = "{}[]()"; //srt = {()
-var z = "([{{}}])"; //srt = {()
-var a = "{}}"; //srt = {()
+var x = "{{}}"; 
+var y = "{}[]()"; 
+var z = "([{{}}])"; 
+var a = "{}}"; 
 
 //Output values
 console.log(parenthesisChecker(x)); //true
